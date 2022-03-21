c2419943f25cd898bfec1178c762b7bb279fbf79
mohamed benzaied <benzaiedmohamed0@gmail.com>
2022-03-21 14:19:15 +0100

    admin interface added

---
+++ frontend/src/Pages/ClientPage.js
@@ -0,0 +1,47 @@
+import '../Assets/CSS/ClientPage.css';
+import Axios from 'axios';
+import {useEffect, useState} from 'react'
+import Footer from '../Components/Footer';
+import Navbar from '../Components/Navbar';
+import Home from "./Home_Page";
+import TelephonePage from './TelephonePage';
+import TablettePage from './TablettePage';
+import OneProductPage from '../Components/OneProductPage';
+import AccessoriesPage from './AccesoriesPage';
+import ComputerPage from './ComputerPage';
+import ComposantPage from './ComposantPage';
+
+function ClientPage() {
+  const [produits,setProduits] = useState([])
+  const [produit,setProduit] = useState([])
+  const [prix,setPrix] = useState(0)
+
+
+  useEffect(() => {
+    let unmounted = false
+    if (!unmounted) {
+        Axios.get("http://localhost:5000/get").then(
+            (data) => {
+                console.log(data)
+                setProduits(data.data)
+            }
+        )
+    }
+    return () => { unmounted = true }
+}, [])
+
+ console.log(produits)
+  produits.map(produit => (
+    console.log(produit.nom_produit)
+  ))
+
+  return (
+    <div className="ClientPage">
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default ClientPage;
